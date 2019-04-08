a0e400d1f89e94866046a7d2338924e11511226f
guoyanling <694034659@qq.com>
2019-04-08 18:34:43 +0800

    Initial commit

---
+++ src/main/java/strategy/fly/FlyNoWay.java
@@ -0,0 +1,8 @@
+package strategy.fly;
+
+public class FlyNoWay implements FlyBehavior{
+
+    public void fly() {
+        System.out.println("不会飞行");
+    }
+}
---
+++ src/main/java/strategy/fly/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package strategy.fly;
+
+public class FlyRocketPowered implements FlyBehavior{
+
+    public void fly() {
+        System.out.println("是火箭动力飞行");
+    }
+}
